cf5217af83a76ddc26297ec52036026cc9c3602a
UTSAB UTSHO <utsab8008@gmail.com>
2020-11-30 06:38:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict'
+ 
+const fastify = require('fastify')({ logger: true })
+const PORT = process.env.PORT || 3000;
+fastify.register(require('fastify-mailer'), {
+  defaults: { from: 'MadvertLabs <madmailserver@gmail.com>' },
+  transport: {
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: process.env.USERNAME,
+      pass: process.env.PASSWORD
+    },
+  }
+})
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
+fastify.post('/api/send', (request, reply) => {
+
+  const { mailer } = fastify
+  const {to,subject,msg,token} = request.body
+  if(typeof(token)=="undefined" || token!=process.env.TOKEN.toString()){
+      return {auth:false}
+  }
+  const res= mailer.sendMail({
+    to: to,
+    subject: subject,
+    text: msg
+  },(errors, info) => {
+      console.log(info)
+    if (errors) {
+      return {
+        status: 'error',
+        message: 'Something went wrong'
+      }
+    }
+    reply.send({
+      status: 'ok',
+      message: 'Email successfully sent!',
+      SentTo:info.envelope.to
+    })
+    return
+  })
+})
+ 
+fastify.listen(PORT,"0.0.0.0", (errors) => {
+  if (errors) {
+    fastify.log.error(errors)
+    process.exit(1)
+  }
+})
